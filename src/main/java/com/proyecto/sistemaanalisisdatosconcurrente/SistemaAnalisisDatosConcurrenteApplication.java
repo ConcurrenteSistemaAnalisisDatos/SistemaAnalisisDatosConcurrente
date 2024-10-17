163344a1ff1209d7ebc951fe1a22b04a568d3b33
Samuu10 <svallmun@myuax.com>
2024-10-17 17:45:40 +0200

    Initial commit

---
+++ src/main/java/com/proyecto/sistemaanalisisdatosconcurrente/SistemaAnalisisDatosConcurrenteApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.sistemaanalisisdatosconcurrente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAnalisisDatosConcurrenteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaAnalisisDatosConcurrenteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/proyecto/sistemaanalisisdatosconcurrente/SistemaAnalisisDatosConcurrenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.sistemaanalisisdatosconcurrente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAnalisisDatosConcurrenteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
